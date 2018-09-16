1b8fb3f058d81228b8d383b6a0589c3786e45578
hanif <abuhanifa@pathao.com>
2018-09-16 21:30:38 +0600

    added-all

--- cmd/config.go
+++ cmd/config.go
@@ -11,7 +11,7 @@ func ConfigCommand() *cobra.Command {
 		Use:   "config",
 		Short: "Get current config name",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Println("---- Current Config ----XXX --- goo ggoo ")
+			log.Println("----afas Current Config ----XXX --- goo ggoo ")
 		},
 	}
 }
