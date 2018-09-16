35bc1eea833077131cb6bf9a2dac993920fabb4d
hanif <abuhanifa@pathao.com>
2018-09-16 21:11:36 +0600

    add config file

--- cmd/config.go
+++ cmd/config.go
@@ -1,1 +1,17 @@
 package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func ConfigCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "config",
+		Short: "Get current config name",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Println("---- Current Config ----")
+		},
+	}
+}
